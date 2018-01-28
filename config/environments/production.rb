dd351f740fbdc854fcb74c0b9529cca858922bf4
Shivam Kapoor <shivamkapoor9999@gmail.com>
2018-01-28 13:53:39 +0530

    debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb. 
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = true 
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -74,7 +74,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  
+  config.action_mailer.perform_deliveries = true
+
+
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
@@ -82,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  
+  config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
@@ -92,5 +94,5 @@ config.action_mailer.smtp_settings = {
   authentication:       'plain',
   enable_starttls_auto: true  }
 
-   config.action_mailer.default_url_options = { host: 'shallsocial.herokuapp.com', protocol: 'https' } 
+   config.action_mailer.default_url_options = { host: 'shallsocial.herokuapp.com', protocol: 'http' } 
 end
